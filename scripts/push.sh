6a360614e12f9fe8343cbfc3108d1c882f964203
Artur Zagozdzinski <aza@markedskraft.com>
2017-11-14 11:11:59 +0100

    2

--- scripts/push.sh
+++ scripts/push.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://96c16dbab9bcb058126eea85f9d851a0ef5272de@github.com/artzag/sampleproject.git > /dev/null 2>&1
+  git remote add origin-pages https://${GH_TOKEN}@github.com/artzag/sampleproject.git > /dev/null 2>&1
   git push --quiet --set-upstream origin master
 }
 
