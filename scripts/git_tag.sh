85f6cdfc2f4942e1090c1594bec24b7ea4d42fa6
Artur Zagozdzinski <azagozdzinski@gmail.com>
2017-11-17 11:40:33 +0100

    Script fix

--- scripts/git_tag.sh
+++ scripts/git_tag.sh
@@ -2,16 +2,17 @@
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
 export VERSION_NO=$(python -c 'from sample import VERSION as version; print(version)');
+export APP_NAME=sample
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 git config --global push.default simple
-export GIT_TAG=sample-$VERSION_NO
+export GIT_TAG=$APP_NAME-$VERSION_NO
 git fetch --tags
 msg="Tag Generated from TravisCI on branch $TRAVIS_BRANCH for build $TRAVIS_BUILD_NUMBER ($YEAR-$MONTH)"
 echo $msg
 echo $GIT_TAG
 #if git tag $GIT_TAG -a -m "$msg" 2>/dev/null; then
 git tag $GIT_TAG -a -m "$msg"
- git push origin travis && git push origin travis --tags
+ git push origin $TRAVIS_BRANCH && git push origin $TRAVIS_BRANCH --tags
 #ls -aR
 #else echo Tag already exists!; fi
